c1eff9b09ea92f1d292e21aa392df3277a2f629c
SreejaReddy-K <sreejareddykattekommula@gmail.com>
2026-05-28 16:46:05 +0530

    pulled

--- srv/skillsphere-service.js
+++ srv/skillsphere-service.js
@@ -1391,8 +1391,15 @@ ${certifications.length > 30 ? `... and ${certifications.length - 30} more certi
    */
   this.before('CREATE', 'InitiativesMaster', async (req) => {
     ensureUuidKey(req, 'initiativeId');
+    req.data.creatorName = await _resolveActorDisplayName(req);
     req.data.createdAt = req.data.createdAt || new Date().toISOString();
     req.data.lastUpdated = new Date().toISOString();
+  });
+
+  this.before('UPDATE', 'InitiativesMaster', async (req) => {
+    if (Object.prototype.hasOwnProperty.call(req.data, 'creatorName')) {
+      delete req.data.creatorName;
+    }
   });
 
   /**
@@ -1400,8 +1407,15 @@ ${certifications.length > 30 ? `... and ${certifications.length - 30} more certi
    */
   this.before('CREATE', 'EvaluationsMaster', async (req) => {
     ensureUuidKey(req, 'evaluationId');
+    req.data.creatorName = await _resolveActorDisplayName(req);
     req.data.createdAt = req.data.createdAt || new Date().toISOString();
     req.data.lastUpdated = new Date().toISOString();
+  });
+
+  this.before('UPDATE', 'EvaluationsMaster', async (req) => {
+    if (Object.prototype.hasOwnProperty.call(req.data, 'creatorName')) {
+      delete req.data.creatorName;
+    }
   });
 
   /**
